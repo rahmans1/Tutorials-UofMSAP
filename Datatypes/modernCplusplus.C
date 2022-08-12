cd4031436c85d4a205c2659ef9e70307e3dc99bb
qunib <107641136+qunib@users.noreply.github.com>
2022-08-12 10:16:56 -0500

    Removal of bad naming of variables in C++

--- Datatypes/modernCplusplus.C
+++ Datatypes/modernCplusplus.C
@@ -2,7 +2,6 @@ int main(){
   int number_of_students = 2; 
   int numberOfStudent = 2;    
   int nos = 2;                // Fix Me: Don't do this. Remove this line of code. 
-  int nos = 2;                // Fix Me: Don't do this. Remove this line of code. 
   
 return 0; 
 }
