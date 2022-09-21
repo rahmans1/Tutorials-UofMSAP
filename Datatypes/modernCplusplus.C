da26a1c3a2ac50785539bf6b8679417542fd8d34
Nazanin Roshanshah <50715010+naz1370@users.noreply.github.com>
2022-09-21 13:40:41 -0400

    Add Short type

--- Datatypes/modernCplusplus.C
+++ Datatypes/modernCplusplus.C
@@ -2,7 +2,8 @@ int main(){
   int number_of_students = 2; 
   int number_of_chairs = 25; //number of chairs in classroom 
   int number_of_benches = 23; // number of benches in the classroom
-  short number_of_students_short = 32767; //numberb of students of data types short
+  short number_of_students_short = 32767; //numberb of students of// data types short
+  
   
 return 0; 
 }
