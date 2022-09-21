a385832f3fea7322976ce216d295216ac8c70b72
JabiaMoazam <89591657+JabiaMoazam@users.noreply.github.com>
2022-09-21 12:44:27 -0500

    change the number of students short and long

--- Datatypes/modernCplusplus.C
+++ Datatypes/modernCplusplus.C
@@ -2,6 +2,8 @@ int main(){
   int number_of_students = 2; 
   int number_of_chairs = 25; //number of chairs in classroom 
   int number_of_benches = 23; // number of benches in the classroom
+  long number_of_students_long = 897654325; // number of students long
+  short number_of_students_short = 235;  // number of short students
   
 return 0; 
 }
