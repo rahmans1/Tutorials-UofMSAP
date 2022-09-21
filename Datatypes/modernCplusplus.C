498d61984900c00c6025c292090fb7982af294a3
Sakib Rahman <rahmans@myumanitoba.ca>
2022-09-21 12:40:39 -0500

    Adding examples of more data types in C++ tutorial

--- Datatypes/modernCplusplus.C
+++ Datatypes/modernCplusplus.C
@@ -1,7 +1,10 @@
 int main(){ 
+     
   int number_of_students = 2; 
-  int number_of_chairs = 25; //number of chairs in classroom 
+  int number_of_chairs = 25; // number of chairs in classroom 
   int number_of_benches = 23; // number of benches in the classroom
+  long number_of_students_long = 9223372036854775807; // number of students of datatype long
+  short number_of_students_short = 32767; // number of students of datatype short
   
 return 0; 
 }
