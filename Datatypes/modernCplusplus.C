af5289dbe70fe6a38279b29f55ea1d828aa6e510
JabiaMoazam <89591657+JabiaMoazam@users.noreply.github.com>
2022-09-28 12:53:24 -0500

    print the the number of students

--- Datatypes/modernCplusplus.C
+++ Datatypes/modernCplusplus.C
@@ -1,3 +1,10 @@
+void printvalues(int i) {
+  std::cout<<i<<std::endl;  
+  return;
+}
+
+
+
 int main(){ 
      
   int number_of_students = 2; 
@@ -5,6 +12,6 @@ int main(){
   int number_of_benches = 23; // number of benches in the classroom
   long number_of_students_long = 897654325; // number of students long
   short number_of_students_short = 32767; //number of students of data types short
-   
+  printvalues(number_of_students); //number of students in class
 return 0; 
 }
